f1436dffe32e72811aeaa2ff8a1847745216fc2e
semaj-james <haileyjames@college.harvard.edu>
2017-01-17 13:04:59 -0500

    games controller file

---
+++ api/controllers/games.js
@@ -0,0 +1,79 @@
+const Game = require('../models/schemas/Game');
+
+exports.createGame = (req, res, next) => {
+
+    // does this check if they exist?
+    if (typeof req.body.email !== 'string')
+        return res.status(400).send('No email');
+    if (typeof req.body.gameCode !== 'string')
+        return res.status(400).send('No gameCode');
+    if (typeof req.body.password !== 'string')
+        return res.status(400).send('No password');
+
+    var gameData = {};
+
+    // http://emailregex.com
+    if (req.body.email) {
+        if (!(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(req.body.email)))
+            return res.status(400).send('Invalid email');
+        else
+            gamedata.email = req.body.email;
+    }
+
+    gameData.gameCode = req.body.gameCode;
+    gameData.password = req.body.password;
+
+    if (req.body.gameStatus) {
+        gameData.gameStatus = req.body.gameStatus;
+    } else {
+        gameData.gameStatus = 0;
+    }    
+    var newGame = new Game(gameData);
+    newGame.save((err, game) => {
+        if (err) {
+            if (err.code === 11000)
+                return res.status(400).send('Game code already registered');    
+            return next(err);
+        }
+        return res.sendStatus(200);
+    });
+}
+
+exports.getAllGames = (req, res, next) => {
+    Game.find({}, (err, games) => {
+        if (err) return next(err);
+        res.json(games);
+    });
+};
+
+exports.getGameById = (req, res, next) => {
+    Game.findById(req.params.id, (err, game) => {
+        if (err) return next(err);
+        if (!game) return res.status(404).send('No game with that ID');
+        res.json(game);
+    });
+};
+
+exports.getActiveGames = (req, res, next) => {
+    Game.find({gameStatus: 1}, (err, users) => {
+        if (err) return next(err);
+        res.json(users);
+    });
+};
+
+exports.updateGameById = (req, res, next) => {
+    User.findOneAndUpdate(req.params.id, req.body, (err, user) => {
+        if (err) return next(err);
+        if (!user) return res.status(404).send('No game with that ID');
+        return res.sendStatus(200);
+    });
+};
+
+
+exports.deleteGameById = (req, res, next) => {
+    Game.findOneAndRemove(req.params.id, (err, user) => {
+        if (err) return next(err);
+        if (!user) return res.status(404).send('No game with that ID');
+        return res.sendStatus(200);
+    });
+}
