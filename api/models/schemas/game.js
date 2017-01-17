3ec7f368ad926a861490f4839cff1ac97e134f89
Tiffany Yu <tiffanyyu268@gmail.com>
2017-01-17 15:30:52 -0500

    Updated game schema to include arrays of players

--- api/models/schemas/game.js
+++ api/models/schemas/game.js
@@ -1,13 +1,38 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 const validator = require('email-validator');
 
+// child schema
+var Player = new Schema({
+        firstName: {type: String, trim: true, required: true},
+        lastName: {type: String, trim: true, required: true},
+        email: {type: String, required: true},
+        password: {type: String, required: true},
+        target: [{Target: {target: Schema.ObjectId, timeAssigned: Date, timeKilled: Date}}],
+        isLiving: Boolean,
+        token: String
+    },
+    {
+        toObject: {getters: true},
+        timeStamps: {
+            createdAt: 'createdDate',
+            updatedAt: 'updatedDate'
+        },
+    }
+});
 
-// create game schemas
+// parent schema
+// game status: 0 sign-up, 1 active, 2 done
 var gameSchema = new Schema({
-    email: {type: String, trim: true},
-    gameStatus: {type: String, trim: true},
-    gameCode: {type: String, unique: true, trim: true},
+    email: {type: String, trim: true, required: true},
+    password: {type: String, trim: true, required: true},
+    gameStatus: {type: Number, trim: true},
+    gameCode: {type: String, required: true, unique: true, trim: true},
+    startDate: Date,
+    livingPlayers: [Player],
+    killedPlayers: [Player],
+    token: String
     },
     {
     toObject:{getters: true},
@@ -22,11 +47,28 @@ gameSchema.pre('save', function(callback){
         return callback(new Error('Missing email'));
     if (this.email && !validator(this.email))
         return callback(new Error('Invalid email'));
+    if (!this.password)
+        return callback(new Error('Missing password'));
+    if (this.isModified('password'))
+        this.hash = bcrypt.hashSync(this.password);
+
+    callback();
+};
+
+gameSchema.methods.comparePassword = function(pw, callback) {
+    bcrypt.compare(pw, this.password, (err, isMatch) => {
+        if(err) return callback(err);
+        callback(null, isMatch);
+    });
+};
+
+Player.methods.comparePlayerPassword = function(pw, callback) {
+    bcrypt.compare(pw, this.password, (err, isMatch) => {
+        if (err) return callback(err);
+        callback(null, isMatch);
+    });
 };
 
 var Game = mongoose.model('Game', gameSchema);
 
 module.exports = Game;
-
-                       
-
--- api/models/schemas/player.js
+++ api/models/schemas/player.js
@@ -1,3 +1,7 @@
+/*
+we don't use this file anymore, but I guess it'll be pushed anyway cause I'm too lazy to figure out git
+*/
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt-nodejs');
@@ -7,10 +11,9 @@ var playerSchema = new Schema({
         firstName: {type: String, trim: true, required: true},
         lastName: {type: String, trim: true, required: true},
         email: {type: String, required: true},
-        hash: {type: String, required: true},
-        gameCode: {type: String, required: true},
+        password: {type: String, required: true},
         target: [{Target: {target: Schema.ObjectId, timeAssigned: Date, timeKilled: Date}}],
-        isLiving: Boolean
+        isLiving: Boolean,
         token: String
     },
     {
@@ -23,29 +26,11 @@ var playerSchema = new Schema({
 
 });
 
-playerSchema.pre('save', function(callback){
-    if (!this.email && !validator.validate(this.email))
-        return callback(new Error('No Email'));
-    if(!this.hash)
-        return callback(new Error('No Password'));
-    if(!this.game)
-        return callback(new Error('Not Registered For a Game'));
-    
-    callback();
-});
-
 playerSchema.methods.comparePassword = function(pw, callback) {
-    bcrypt.compare(pw, this.hash, (err, isMatch) => {
+    bcrypt.compare(pw, this.password, (err, isMatch) => {
         if(err) return callback(err);
         callback(null, isMatch);
     })
 };
 
-var Player = mongoose.model('Player', playerSchema);
-
-module.exports = Player;
-
-
-
-
-
+module.exports = playerSchema;
