644ea84e3007943421cbf43cd59c0baff5296efa
Dong Hur <ushur2016@gmail.com>
2017-01-18 10:12:55 -0500

    pulled and added adming pug and js

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
 const request = require('request');
 const config = require('../app/models/config');
+const auth = require('./auth');
 
 router.get('/', (req, res, next) => {
     return res.render('index', { title: 'Home' });
+});
+
+router.get('/admin', (req, res, next) => {
+    return res.render('admin', { title: 'Admin' });
 });
 
 module.exports = router;
