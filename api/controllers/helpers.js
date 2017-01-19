eef104fa4b9363544c9cf6905cb7eba37e514ed2
Tiffany Yu <tiffanyyu268@gmail.com>
2017-01-19 16:53:46 -0500

    switched to mongoose subdoc lookup

--- api/controllers/helpers.js
+++ api/controllers/helpers.js
@@ -2,7 +2,6 @@ const schemas = require('../models/schemas/game');
 const Game = schemas[0];
 const Player = schemas[1];
 
-// TODO: find using subdocuments
 exports.findPlayerById = (gameCode, id, callback) => {
     Game.findOne({ gameCode: gameCode }, (err, game) => {
         if (err) return callback(err);
@@ -10,18 +9,11 @@ exports.findPlayerById = (gameCode, id, callback) => {
             var e = new Error('No game with that game code');
             e.status = 404;
             return callback(e);
-        };
-        for (var i = 0; i < game.allPlayers.length; i++) {
-            if (String(game.allPlayers[i]._id) === id) {
-                var player = game.allPlayers[i];
-            }
-        };
-        if (!player) {
-            callback();
-        } else {
-            callback(null, player, game);
         }
-    });
+        var player = game.allPlayers.id(id);
+        if (!player) callback();
+        else callback(null, player, game);
+    })
 };
 
 exports.findPlayerByEmail = (gameCode, email, callback) => {
@@ -34,8 +26,9 @@ exports.findPlayerByEmail = (gameCode, email, callback) => {
         };
         for (var i = 0; i < game.allPlayers.length; i++) {
             if (String(game.allPlayers[i].email) === email)
-                return callback(null, game.allPlayers[i], game);
+                var player = game.allPlayers[i];
         };
-        callback(null);
+        if (!player) callback();
+        else callback(null, player, game);
     });
 };
